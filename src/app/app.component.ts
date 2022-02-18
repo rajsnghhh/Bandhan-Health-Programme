c9e9c1ce14bee44111fa395d56d95067233da8a9
Chayan Datta <107796@bk.local>
2022-02-18 18:06:53 +0530

    pull

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ValidationService } from './modules/shared/services/validation.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'bhp-web';
-  abc: boolean = false;
 
+  constructor(public validationService: ValidationService){}
+
+  ngOnInit(): void {
+  }
+  
+  
 }
--- src/app/modules/shared/sidebar/sidebar.component.ts
+++ src/app/modules/shared/sidebar/sidebar.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, DoCheck, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { ValidationService } from '../services/validation.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit{
 
   hide:boolean = true;
   hideSideNav: boolean = false;
-  constructor() { }
+
+  constructor(public validationService: ValidationService) { }
 
   ngOnInit(): void {
+    
   }
+
 
   hideSideBar(){
     this.hideSideNav = !this.hideSideNav;
     this.hide = !this.hide;
   }
-
 
 }
