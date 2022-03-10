c49c31719256da122872325110541ba49b75bcc7
ChayanDatta <chayan.datta@bandhan.org>
2022-03-10 18:08:02 +0530

    login trim

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -59,7 +59,7 @@ export class LoginComponent implements OnInit {
     this.loading = false;
 
     setTimeout(() => {
-      this.accountService.login(("BK" + this.f.username.value), this.f.password.value)
+      this.accountService.login(("BK" + this.f.username.value).trim(), this.f.password.value)
         .pipe(first())
         .subscribe(
           (data: RootObject) => {
