231ef89451db5c6de047931f13d55fece8c59b01
ChayanDatta <chayan.datta@bandhan.org>
2022-04-07 09:59:29 +0530

    pw date issue fix

--- src/app/modules/pregnant-women-register/pw-view/pw-view.component.ts
+++ src/app/modules/pregnant-women-register/pw-view/pw-view.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import * as moment from 'moment';
 import { ToastrService } from 'ngx-toastr';
 import { HttpService } from '../../core/http/http.service';
 import { ValidationService } from '../../shared/services/validation.service';
@@ -24,6 +25,8 @@ export class PwViewComponent implements OnInit {
   actualDeliveryDate: any;
   MotherDeath: any;
   acMode: boolean;
+  miscarriage: boolean;
+  abortion: boolean;
 
   constructor(private http: HttpClient, private httpService: HttpService, private fb: FormBuilder,
     public validationService: ValidationService, private toaster: ToastrService, private sidebarService: SidebarService,
@@ -70,7 +73,7 @@ export class PwViewComponent implements OnInit {
 
   createForm() {
     this.pwRegisterForm = this.fb.group({
-      initialWeight: [null],
+      initialWeight: [null, this.weightRange],
       lastMenstrualDate: [null],
       expectedDeliveryDate: [null],
       ancComplete: [null],
@@ -79,8 +82,9 @@ export class PwViewComponent implements OnInit {
       anc3rd: [null],
       anc4th: [null],
       pregnancyComplication: [''],
-      beforeDeliveryWeight: [null],
+      beforeDeliveryWeight: [null, this.weightRange],
       delivery: [''],
+      deliveryNo: [''],
       miscarriage: [null],
       abortion: [null],
       actualDeliveryDate: [''],
@@ -95,10 +99,22 @@ export class PwViewComponent implements OnInit {
     return this.pwRegisterForm.controls;
   }
 
+  weightRange(controls: AbstractControl): { [key: string]: any } | null {
+    if (controls.value >= 20 && controls.value <= 200 || controls.value == null) {
+      return null;
+    }
+    return { 'notInWeightRange': true };
+  }
+
   restrictAncDate(value) {
-    this.actualDeliveryDate = value;
+    this.actualDeliveryDate = moment(new Date().setDate(new Date(value).getDate() + 1)).format('YYYY-MM-DD');
+    this.AncDate = moment(new Date().setDate(new Date(value).getDate() + 1)).format('YYYY-MM-DD');
+    this.pwRegisterForm.get('expectedDeliveryDate').reset();
+    this.pwRegisterForm.get('anc1st').reset();
+    this.pwRegisterForm.get('anc2nd').reset();
+    this.pwRegisterForm.get('anc3rd').reset();
+    this.pwRegisterForm.get('anc4th').reset();
     this.pwRegisterForm.get('actualDeliveryDate').reset();
-    this.AncDate = new Date(new Date().setDate(new Date(value).getDate() + 1)).toISOString().substring(0, 10);
   }
 
   checkAnc(value) {
@@ -119,6 +135,19 @@ export class PwViewComponent implements OnInit {
     } else {
       this.deliveryStatusNo = false;
       this.deliveryStatusYes = false;
+    }
+  }
+
+  checkDeliveryStatusNo(value) {
+    if (value == 'miscarriage') {
+      this.miscarriage = true;
+      this.abortion = false;
+    } else if (value == 'abortion') {
+      this.miscarriage = false;
+      this.abortion = true;
+    } else {
+      this.miscarriage = false;
+      this.abortion = false;
     }
   }
 
