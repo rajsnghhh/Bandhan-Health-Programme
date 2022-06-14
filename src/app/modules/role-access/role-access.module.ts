b6ea2094b868b93dc1367b6b3f770e1995f84ae6
Rajmani Singh <106630@bk.local>
2022-06-14 11:14:38 +0530

    chng

--- src/app/modules/role-access/role-access.module.ts
+++ src/app/modules/role-access/role-access.module.ts
@@ -3,18 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RoleAccessRoutingModule } from './role-access-routing.module';
 import { RoleAccessComponent } from './role-access.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatExpansionModule } from '@angular/material/expansion';
-
 
 @NgModule({
   declarations: [RoleAccessComponent],
   imports: [
     CommonModule,
     RoleAccessRoutingModule,
-    ReactiveFormsModule,
-    MatExpansionModule,
-    
+    ReactiveFormsModule  
   ]
 })
 export class RoleAccessModule { }
