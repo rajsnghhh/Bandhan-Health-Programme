d7215fa08fcddb6765fb2316766fb70222f464d3
ChayanDatta <chayan.datta@bandhan.org>
2022-07-18 18:08:53 +0530

    Dashboard Design

--- src/app/modules/core/core.component.ts
+++ src/app/modules/core/core.component.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Chart } from 'chart.js'
+import { ToastrService } from 'ngx-toastr';
+import { HttpService } from './http/http.service';
 @Component({
   selector: 'app-core',
   templateUrl: './core.component.html',
@@ -12,14 +16,86 @@ export class CoreComponent implements OnInit, AfterViewInit {
   canvas2: any;
   ctx2: any;
   @ViewChild('mychart2') mychart2: any;
-
-  constructor() { }
+  loader: boolean = true;
+  Dto = {
+    dataAccessDTO: this.httpService.dataAccessDTO,
+  }
+  totalDonor: any;
+  totalBlock: any;
+  totalBranch: any;
+  totalDistrict: any;
+  totalProjectMasterId: any;
+  totalRegion: any;
+  totalStateName: any;
+  totalVillageLocality: any;
+  totalFamilyCount: any;
+  totalPemCumulative: any;
+  totalLmCumulative: any;
+  totalPwCumulative: any;
+  constructor(private httpService: HttpService, public dialog: MatDialog,
+    private http: HttpClient, private toaster: ToastrService,) { }
 
   ngOnInit() {
+    this.loader = false;
+
+    this.http.post(`${this.httpService.baseURL}report/getGeographicalOutreach`, this.Dto).subscribe((res: any) => {
+      let donorName: Array<any> = [];
+      let block: Array<any> = [];
+      let branch: Array<any> = [];
+      let district: Array<any> = [];
+      let projectMasterId: Array<any> = [];
+      let region: Array<any> = [];
+      let stateName: any = [];
+      let villageLocality: Array<any> = [];
+      res.responseObject?.geographicalOutreachList.map(i => {
+        donorName.push(i.donorName);
+        block.push(i.block);
+        branch.push(i.branch);
+        district.push(i.district);
+        projectMasterId.push(i.projectMasterId);
+        region.push(i.region);
+        stateName += i.stateName + ',';
+        villageLocality.push(i.villageLocality);
+      })
+      let arr = new Array();
+      arr = stateName.slice(0, (stateName.length - 1)).split(",");
+      let removeNullState = arr.filter((i) => i != 'null');
+      this.totalDonor = [...new Set(donorName)].length;
+      this.totalBlock = block.reduce((a, b) => a + b, 0);
+      this.totalBranch = branch.reduce((a, b) => a + b, 0);
+      this.totalDistrict = district.reduce((a, b) => a + b, 0);
+      this.totalProjectMasterId = [...new Set(projectMasterId)].length;
+      this.totalRegion = region.reduce((a, b) => a + b, 0);
+      this.totalStateName = [...new Set(removeNullState)].length;
+      this.totalVillageLocality = villageLocality.reduce((a, b) => a + b, 0);
+    }, error => {
+    });
+
+
+
   }
 
   ngAfterViewInit(): void {
-    this.doughnutChart();
+    this.http.post(`${this.httpService.baseURL}report/getBeneficiaryInfoProject`, this.Dto).subscribe((res: any) => {
+      let familyCount: Array<any> = [];
+      let pemCumulative: Array<any> = [];
+      let lmCumulative: Array<any> = [];
+      let pwCumulative: Array<any> = [];
+      res.responseObject.projectWiseBeneficiaryList.map((i) => {
+        familyCount.push(i.totalFamilyCount);
+        pemCumulative.push(i.pemCumulative);
+        lmCumulative.push(i.lmCumulative);
+        pwCumulative.push(i.pwCumulative);
+      })
+      this.totalFamilyCount = familyCount.reduce((a, b) => a + b, 0);
+      this.totalPemCumulative = pemCumulative.reduce((a, b) => a + b, 0);
+      this.totalLmCumulative = lmCumulative.reduce((a, b) => a + b, 0);
+      this.totalPwCumulative = pwCumulative.reduce((a, b) => a + b, 0);
+      console.log(this.totalFamilyCount, this.totalPemCumulative, this.totalLmCumulative, this.totalPwCumulative);
+      this.doughnutChart();
+      this.loader = true;
+    });
+
     this.barChart();
 
   }
@@ -30,16 +106,19 @@ export class CoreComponent implements OnInit, AfterViewInit {
 
     const doughnutdata = {
       labels: [
+        'Total Family',
         'PEM',
         'LM',
-        'PW'
+        'PW',
       ],
       datasets: [{
-        data: [300, 50, 100],
+        data: [this.totalFamilyCount, this.totalPemCumulative, this.totalLmCumulative, this.totalPwCumulative],
         backgroundColor: [
+          'rgb(75, 192, 192)',
           'rgb(245, 57, 97)',
           'rgb(20, 154, 245)',
-          'rgb(255, 189, 32)'
+          'rgb(255, 189, 32)',
+
         ],
         hoverOffset: 4
       }]
@@ -73,8 +152,15 @@ export class CoreComponent implements OnInit, AfterViewInit {
       }, {
         label: 'Child PEM',
         data: [80],
-        backgroundColor: "rgba(255, 205, 86, 0.5)",
+        backgroundColor: "rgba(255, 205, 86, 0.6)",
         borderColor: 'rgb(255, 205, 86)',
+        borderWidth: 2
+      }
+        , {
+        label: 'Adolescent Girls',
+        data: [64],
+        backgroundColor: "rgba(54, 162, 235, 0.5)",
+        borderColor: 'rgb(54, 162, 235)',
         borderWidth: 2
       }
       ]
