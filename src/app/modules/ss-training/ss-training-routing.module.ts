c325b55ab95470e8cba817a3742df70418fc5d17
Rajmani Singh <106630@bk.local>
2022-08-23 17:50:02 +0530

    chng

--- src/app/modules/ss-training/ss-training-routing.module.ts
+++ src/app/modules/ss-training/ss-training-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { SsTrainingComponent } from './ss-training.component';
 
 const routes: Routes = [{ path: '', component: SsTrainingComponent }];
--- src/app/modules/ss-training/ss-training.module.ts
+++ src/app/modules/ss-training/ss-training.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SsTrainingRoutingModule } from './ss-training-routing.module';
 import { SsTrainingComponent } from './ss-training.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
 
 @NgModule({
   declarations: [SsTrainingComponent],
